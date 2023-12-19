bec31ad52b18839b48f9d5b13c8b4b423f79b4f8
JerGrant <jegracollege@gmail.com>
2023-12-19 12:35:46 -0700

    app User model

---
+++ Gym-Logger/Data/appUser.cs
@@ -0,0 +1,19 @@
+﻿namespace Gym_Logger.Data
+{
+    public class appUser
+    {
+        // Identifiers
+        public string UserName { get; set; }
+        public string ProfileImg? {  get; set; }
+        // User Attributes
+        public double Height? { get; set; }
+        public double Weight? { get; set; }
+        public double GoalWeight? { get; set; }
+        public int DailyCalories? { get; set; }
+        public double BMI? {  get; set; }
+        // Gameify fields
+        public int WorkoutsCompleted? { get; set; }
+        public double HoursWorkedOut? { get; set; }
+        public double IbsLifted? {  get; set; }
+    }
+}
